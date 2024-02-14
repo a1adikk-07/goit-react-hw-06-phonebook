9b0f2c8f3d1be282de1d72af5cc19345ff71ff75
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-02-14 12:51:05 +0200

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist';
+// import { persistStore } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import rootReducer from './rootReducer';
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persiste = persistStore(store);
